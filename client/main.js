5910e6644567cb542d16029ca5ebff3d8b586df0
Samuel <samuel.linehan15@gmail.com>
2016-05-11 16:05:58 -0400

    divided json data and found temperatures

--- client/main.js
+++ client/main.js
@@ -1,9 +1,45 @@
 $(document).ready(function() {
-  getWeatherData();
-})
+  getWeatherData().then(function(data) {
+    var weatherData = JSON.parse(data);
+    var fiveDays = [];
+    var daysData = weatherData.list;
+
+    var weatherDataIndex = 0;
+    for(var i = 0; i <= 4; i++) {
+      var daySection = daysData.slice(weatherDataIndex, weatherDataIndex + 8)
+      // console.log(daySection);
+      temperatures(daySection).maxTemp;
+      temperatures(daySection).minTemp;
+      // var newDay = {
+      //   time: weatherData[weatherDataIndex].dt,
+      //   minTemp:
+      //   maxTemp: ,
+      //   description: ,
+      //   icon: ,
+      // }
+      weatherDataIndex += 8;
+    }
+  })
+});
 
 function getWeatherData() {
-  return $.get("http://api.openweathermap.org/data/2.5/forecast/city?id=4940625&APPID=6ace34d5c1936f6a6027b0e25ffa060e", function(data) {
-    console.log(data);
+  return $.get("http://localhost:3000/data", function(data) {
+    return data;
   })
+}
+
+function temperatures(daysData) {
+  var temperaturesArray = [];
+  for(var i = 0; i < daysData.length; i ++) {
+    temperaturesArray.push(daysData[i].main.temp);
+  }
+  // console.log(temperaturesArray);
+  console.log({
+    maxTemp: Math.max.apply(Math, temperaturesArray),
+    minTemp: Math.min.apply(Math, temperaturesArray)
+  })
+  return {
+    maxTemp: Math.max.apply(Math, temperaturesArray),
+    minTemp: Math.min.apply(Math, temperaturesArray)
+  };
 }
